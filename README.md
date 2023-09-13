891ba7eac012473ff9828f084a2242fb7848cb32
Bolism233 <bolism233@gmail.com>
2023-09-13 12:07:45 -0400

    got the song name and artist name

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # YTMUSIC
+## How it works:
+1. Takes Playlist URL
+2. Get songs in the playlist
+3. Get song name and artist by splitting song Name - Artist
+4. Obtain the song label/category from lastfm.api?
+
+## Next Steps:
+**Obtain song label/category**
 
--- main.py
+++ main.py
@@ -1,34 +1,61 @@
-import os
+import requests
+import json
+from credentials import api_key
 
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
+### Obtain User Playlist
+""" playlist_link = input("Enter your playlist link: ") """
+playlist_link = "https://youtube.com/playlist?list=PLTOXykx0lDH5nrxwnRBgH4d1ZbiBMUTPg&si=G9dV9bCgfphEYufK"
+parts = playlist_link.split("list=")
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+# Check if "list=" is found in the URL
+if len(parts) == 2:
+    playlist_id = parts[1]
+    # Remove any additional parameters
+    playlist_id = playlist_id.split("&")[0]
 
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    # Now, playlist_id contains what comes after "list="
+    print("Playlist ID:", playlist_id)
+else:
+    print("Invalid playlist link.")
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "client_secret_.json"
+### Obtain PlayList information via API
+url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={playlist_id}&key={api_key}&maxResults=50"
+response = requests.get(url)
+data = response.json()
+videos = data.get("items", [])
+song_info = {}
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_local_server()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+for video in videos:
+    snippet = video.get("snippet", {})
+    video_title = snippet.get("title", "No Title")
+    video_description = snippet.get("description", "No Description")
+    video_id = video["snippet"]["resourceId"]["videoId"]
 
-    request = youtube.playlists().list(
-        part="snippet",
-        channelId="UCcaC1E421Wjn17vyVBAsF9w"
-    )
-    response = request.execute()
+    print(f"Video Title: {video_title}")
+    """ print(f"Video ID: {video_id}") """
+    parts = video_title.split(" - ")
+    if len(parts) >= 2:
+        artist_name = parts[0]
+        song_name = parts[1]
+        print(f"Artist Name: {artist_name}")
+        print(f"Song Name: {song_name}")
 
-    print(response)
+        song_info.update({song_name: artist_name})
+        
+    else:
+        print("Title format not recognized.")
+    print("\n")
 
-if __name__ == "__main__":
-    main()
+print("Song Info: \n")
+for key,value, in song_info.items():
+    print(f"{key}, {value}")
+
+""" # Serializing json
+json_object = json.dumps(videos, indent=4)
+ 
+# Writing to sample.json
+with open("sample.json", "w") as outfile:
+    outfile.write(json_object)
+ """
+
+# Now, you can work with the 'data' variable to access the video information within the playlist.
