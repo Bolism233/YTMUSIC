1cdaa43946af6d3bd26a5c0840acf797cdfc1ccc
Bolism233 <bolism233@gmail.com>
2023-10-02 10:51:49 -0400

    wip[

--- README.md
+++ README.md
@@ -3,9 +3,13 @@
 1. Takes Playlist URL
 2. Get songs in the playlist
 3. Get song name and artist by splitting song Name - Artist
-    Limitations: Only recognize format: {Artist} - {Song Name} 
+    Limitations: 
+        1. Only recognize format: {Artist} - {Song Name} 
+        2. Can only recognize songs in lastfm database
 4. Obtain the song label/category from lastfm.api?
 
-## Next Steps:
-**Obtain song label/category**
+## Next Step:
+**optimize request code to reduce repetition**
+**Get genre/label of the music from 'track'**
+**Find similar music**
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import requests
-import json
-import pylast
+import requests, json, pylast, re
+from collections import defaultdict
 from credentials import api_key, api_lastfm, api_secret
 
 ### Obtain User Playlist via link
@@ -24,7 +23,7 @@ url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlis
 response = requests.get(url)
 data = response.json()
 videos = data.get("items", [])
-song_info = {}
+song_info = defaultdict(list)
 
 for video in videos:
     snippet = video.get("snippet", {})
@@ -32,73 +31,60 @@ for video in videos:
     video_description = snippet.get("description", "No Description")
     video_id = video["snippet"]["resourceId"]["videoId"]
 
-    print(f"Video Title: {video_title}")
-    """ print(f"Video ID: {video_id}") """
+    """ print(f"Video Title: {video_title}") """
     parts = video_title.split(" - ")
     if len(parts) >= 2:
         artist_name = parts[0]
         song_name = parts[1]
-        print(f"Artist Name: {artist_name}")
-        print(f"Song Name: {song_name}")
+
+        # Use re.sub to remove the matched text
+        pattern = r'\[.*?\]|\(.*?\)'
+        song_name = re.sub(pattern, '', song_name)
 
         ### store song into song_info
-        song_info.update({song_name: artist_name})
+        song_info[artist_name].append(song_name)
         
     else:
         print("Title format not recognized.")
-    print("\n")
-
-""" print("Song Info: \n")
-for key,value, in song_info.items():
-    print(f"{key}, {value}") """
-
-network = pylast.LastFMNetwork(api_key=api_lastfm, api_secret=api_secret)
-
-# Replace with the artist and song names you're interested in
-artist_name = "Justin Bieber"
-song_title = "What do you Mean"
-
-url = f'http://ws.audioscrobbler.com/2.0/?method=track.search&track={song_title}&artist={artist_name}&api_key={api_lastfm}&format=json'
-
-# Make the GET request
-response = requests.get(url)
-
-# Parse the JSON response
-data = response.json()
-
-# Extract information from the response
-tracks = data['results']['trackmatches']['track']
-
-for track in tracks:
-    print(f"Track: {track['name']} by {track['artist']}")
 
 
-""" # Search for the artist
-artist_results = network.search_for_artist(artist_name)
+for artist_name, song_name in song_info.items():
+    print(f"Artist Name: {artist_name}")
+    print(f"Song Name: {song_name}")
 
-if artist_results:
-    # Get the first artist from the search results
-    artist = artist_results[0]
-
-    # Retrieve a list of the artist's tracks
-    tracks = artist.get_top_tracks()
-
-    # Find the desired song in the list of tracks
-    for track in tracks:
-        if track.title.lower() == song_title.lower():
-            # Get the top tags (genres) associated with the track
-            tags = track.get_top_tags()
-            if tags:
-                genres = [tag.item.name for tag in tags]
-                print(f"Genres for '{song_title}' by {artist_name}: {', '.join(genres)}")
-            else:
-                print("Genre information not available for this song.")
-            break
+for artist_name, song_name in song_info.items():
+    if((len(song_name) > 1) and artist_name == "Ed Sheeran"):
+        for song in song_name:
+            print(f"Artist Name: {artist_name}, Song List: {song}")
+            url = f'http://ws.audioscrobbler.com/2.0/?method=track.search&track={song}&artist={artist_name}&api_key={api_lastfm}&format=json'
+            # Make the GET request
+            response = requests.get(url)
+            # Parse the JSON response
+            data = response.json()
+            # Extract information from the response
+            try:
+                if 'trackmatches' in data['results'] and 'track' in data['results']['trackmatches']:
+                    # Get the first matching track
+                    track = data['results']['trackmatches']['track'][0]
+                    print(f"Track: {track['name']} by {track['artist']}")
+                else:
+                    print("Track not found.")
+            except:
+                print("not working")
+    
     else:
-        print(f"'{song_title}' by {artist_name} not found in top tracks.")
-else:
-    print(f"Artist '{artist_name}' not found.")
- """
-
-
-# Now, you can work with the 'data' variable to access the video information within the playlist.
+        url = f'http://ws.audioscrobbler.com/2.0/?method=track.search&track={song_name}&artist={artist_name}&api_key={api_lastfm}&format=json'
+        # Make the GET request
+        response = requests.get(url)
+        # Parse the JSON response
+        data = response.json()
+        # Extract information from the response
+        try:
+            if 'trackmatches' in data['results'] and 'track' in data['results']['trackmatches']:
+                # Get the first matching track
+                track = data['results']['trackmatches']['track'][0]
+                print(f"Track: {track['name']} by {track['artist']}")
+            else:
+                print("Track not found.")
+        except:
+            print("not working")
